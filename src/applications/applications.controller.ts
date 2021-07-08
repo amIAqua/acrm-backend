a754fa80c9c59267124c7c4c33a4010265a56889
amIAqua <sidelnikov224@gmail.com>
2021-07-08 11:25:15 +0300

    applications/controller/getOne method

--- src/applications/applications.controller.ts
+++ src/applications/applications.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Delete, Param } from '@nestjs/common'
+import { Controller, Delete, Get, Param } from '@nestjs/common'
 import { ApplicationsService } from './applications.service'
 
 @Controller('applications')
 export class ApplicationsController {
   constructor(private applicationsService: ApplicationsService) {}
+
+  @Get('/:id')
+  async getOne(@Param() params: { id: string }) {
+    return this.applicationsService.findByPk(+params.id)
+  }
 
   @Delete('/:id/delete')
   async deleteOne(@Param() params: { id: string }) {
